4e1d24ea0203834bb514321a79d651bb7d8a9454
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:25 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/noc/monitor/callable/CallableTreeListener.java
+++ src/main/java/com/aoindustries/noc/monitor/callable/CallableTreeListener.java
@@ -1,6 +1,6 @@
 /*
  * noc-monitor-callable - Wrapper for implementing hooks and filters on Monitoring API.
- * Copyright (C) 2012, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2012, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with noc-monitor-callable.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.noc.monitor.callable;
 
 import com.aoindustries.noc.monitor.common.AlertLevelChange;
